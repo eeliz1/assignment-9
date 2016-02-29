0142455f68b6a3e08537d705d91e1e3014dd6f71
Pramod Pantha <ppanth1@lsu.edu>
2016-02-29 11:29:47 -0600

    assignment9_submission

---
+++ answers/pramodpantha/task2_asgn9.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# utf-8
+
+"""
+Assignment 9,task2
+Created by Pramod Pantha on 29 Feb, 2016.
+Copyright 2016 Pramod Pantha. All right reserved.
+"""
+
+
+def format_change(ExaMLfile_format):
+    a = ExaMLfile_format.replace('\t', ',')
+    b = a.split('\n')
+    return b
+
+
+def main():
+    ExmALfile_format = """ExaML_info.T6\tLikelihood of best tree:\t-82924194.67\
+    n
+ExaML_info.T5\tLikelihood of best tree:\t-82924194.71\n
+ExaML_info.T9\tLikelihood of best tree:\t-82924194.73\n
+ExaML_info.T7\tLikelihood of best tree:\t-82924252.98\n
+ExaML_info.T2\tLikelihood of best tree:\t-82924253.01\n
+ExaML_info.T1\tLikelihood of best tree:\t-82924354.95\n
+ExaML_info.T8\tLikelihood of best tree:\t-82924354.98\n
+ExaML_info.T15\tLikelihood of best tree:\t-82924366.58\n
+ExaML_info.T0\tLikelihood of best tree:\t-82924366.59\n
+ExaML_info.T4\tLikelihood of best tree:\t-82924397.48\n
+ExaML_info.T16\tLikelihood of best tree:\t-82924424.87\n
+ExaML_info.T3\tLikelihood of best tree:\t-82924424.89\n
+ExaML_info.T14\tLikelihood of best tree:\t-82924426.52\n
+ExaML_info.T12\tLikelihood of best tree:\t-82924426.53\n
+ExaML_info.T13\tLikelihood of best tree:\t-82924426.54\n
+ExaML_info.T19\tLikelihood of best tree:\t-82924465.57\n
+ExaML_info.T17\tLikelihood of best tree:\t-82924707.69\n
+ExaML_info.T10\tLikelihood of best tree:\t-82925366.65\n
+ExaML_info.T18\tLikelihood of best tree:\t-82925393.89\n
+ExaML_info.T11\tLikelihood of best tree:\t-82925447.6\n"""
+    for item in format_change(ExmALfile_format):
+        print(item)
+
+
+if __name__ == '__main__':
+    main()
